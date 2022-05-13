28199a3bd5d681a79727f9353c9c4a4e993eda5c
Dgsbarbosa <guinho.cq@gmail.com>
2022-05-13 19:21:40 -0300

    up

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,5 +1,24 @@
+function handleMouseEnter(){
+    this.classList.add('s-card--hovered');
+    document.body.id = `${this.id}-hovered`; 
+}
+
+function handleMouseLeave(){
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
+}
+
+
 function addEventListennersToCards(){
-    constcardElements = document.getElementsByClassName('s-card')
+    const cardElements = document.getElementsByClassName('s-card')
+
+    for(let index = 0; index < cardElements.length; index++ ){
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter)
+        card.addEventListener('mouseleave', handleMouseLeave)
+
+
+    }
 }
 
 document.addEventListener("DOMContentLoaded", addEventListennersToCards, false)
